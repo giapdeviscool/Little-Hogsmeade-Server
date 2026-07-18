1d8505a811cf15f0d2d76c49c433551d872524d5
anhntm05 <157496091+anhntm05@users.noreply.github.com>
2026-07-18 22:56:57 +0700

    Pos (#22)
    * otp verify recheck
    
    * otp verification
    
    * phan quyen

--- src/routes/menu-item.routes.js
+++ src/routes/menu-item.routes.js
@@ -7,7 +7,7 @@ var imageUploadMiddleware = require('../middlewares/image-upload.middleware');
 var router = express.Router();
 
 router.use(authMiddleware.authenticate);
-router.use(authMiddleware.verifyRole(['owner', 'chain admin', 'manager']));
+router.use(authMiddleware.verifyRole(['owner', 'chain admin', 'cashier']));
 
 
 // UC65: View Menu Items
